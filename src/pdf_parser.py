94414e3761222cd6ca78c6e626761a14db72c2ce
mihirathale98 <athale.m@northeastern.edu>
2024-09-21 10:52:03 -0400

    updated pdf parser

--- src/pdf_parser.py
+++ src/pdf_parser.py
@@ -2,14 +2,13 @@ import PyPDF2
 
 def parse_pdf(pdf_file_path):
 
-    content = ""
+    content = {}
     try:
         with open(pdf_file_path, 'rb') as file:
             pdf_reader = PyPDF2.PdfReader(file)
             print('Parsed PDF')
-            for page in pdf_reader.pages:
-                content += page.extract_text() 
-
+            for i, page in enumerate(pdf_reader.pages):
+                content[i] = {'page_text': page.extract_text()}
         return content
 
     except FileNotFoundError:
